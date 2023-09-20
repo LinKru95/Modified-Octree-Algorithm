7946f953f07c59a4fe00ec0b18122e7550af13ec
LinasKrutkevicius <100715343+LinKru95@users.noreply.github.com>
2023-09-20 20:44:30 +0300

    Revert "Made the algorithm work as expected"
    This reverts commit 56000f37d0245f4c2c412b024a66f65f0462d619.

--- modifyed_octree_algorithm.py
+++ modifyed_octree_algorithm.py
@@ -1,37 +1,39 @@
-import glob
 import laspy
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D 
 from utils import calculate_bounding_box, add_child_nodes, split_cube, filter_points_for_node
 from octree import create_initial_octree_root
 
-las_files = glob.glob("*.las")
+max_depth = 1
 
-if len(las_files) == 0:
-    print("No .las files found in the current directory.")
-else:
-    las_file_path = las_files[0]
-
-with laspy.open(las_file_path, mode="r") as las_file:
+with laspy.open("2743_1234.las", mode="r") as las_file:
     las = las_file.read()
-    x_values = las.x
-    y_values = las.y
+	@@ -11,27 +13,49 @@
     z_values = las.z
 
 points = list(zip(x_values, y_values, z_values))
+points = points[::10000]
+print("Number of points:", len(points))
 bounding_box = calculate_bounding_box(points)
 root_octree_node = create_initial_octree_root(bounding_box)
+# points = [point for point in points if is_point_inside_node(point, root_octree_node)]
 add_child_nodes(root_octree_node, split_cube)
 points = filter_points_for_node(root_octree_node, points)
-points = points[::1000]
+# print("Number of points:", len(points))
+# points = points[::10000]
+print("Number of points:", len(points))
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+
 x_filtered = [point[0] for point in points]
 y_filtered = [point[1] for point in points]
 z_filtered = [point[2] for point in points]
+
 ax.scatter(x_filtered, y_filtered, z_filtered, c='b', marker='.')
+
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 plt.title('Filtered 3D Point Cloud')
+
 plt.show()
--- utils.py
+++ utils.py
@@ -2,9 +2,10 @@ import math
 from octree import OctreeNode
 
 def calculate_bounding_box(points):
-    min_x = min_y = min_z = float('inf')
-    max_x = max_y = max_z = float('-inf')
-
+    # Initialize minimum and maximum coordinates
+    min_x = min_y = min_z = float('inf')  # Initialize to positive infinity
+    max_x = max_y = max_z = float('-inf')  # Initialize to negative infinity
+    # Iterate through the points to find the minimum and maximum coordinates
     for point in points:
         x, y, z = point[0], point[1], point[2]
         min_x = min(min_x, x)
@@ -13,7 +14,6 @@ def calculate_bounding_box(points):
         max_x = max(max_x, x)
         max_y = max(max_y, y)
         max_z = max(max_z, z)
-
     edge_length = max(max_x - min_x, max_y - min_y, max_z - min_z)
     center_x = (max_x + min_x) / 2
     center_y = (max_y + min_y) / 2
@@ -22,20 +22,24 @@ def calculate_bounding_box(points):
     min_corner_y = center_y - edge_length / 2
     min_corner_z = center_z - edge_length / 2
     bounding_cube = ((min_corner_x, min_corner_y, min_corner_z), (min_corner_x + edge_length, min_corner_y + edge_length, min_corner_z + edge_length))
-
     return bounding_cube
 
 def add_child_nodes(node, split_cube):
+    # Split the current cube into child cubes
     child_cubes = split_cube(node)
 
+    # Create child nodes for each cube
     for child_cube in child_cubes:
         child_node = OctreeNode(child_cube.min_corner, child_cube.size)
         node.children.append(child_node)
 
 def split_cube(parent_cube):
+    # Extract parent cube information
     parent_min_corner = parent_cube.min_corner
     parent_size = parent_cube.size
+    # Calculate the half size of the parent cube
     half_size = (parent_size[0] / 2, parent_size[1] / 2, parent_size[2] / 2)
+    # Define the eight child cube positions relative to the parent cube
     child_positions = [
         parent_min_corner,
         (parent_min_corner[0] + half_size[0], parent_min_corner[1], parent_min_corner[2]),
@@ -46,8 +50,8 @@ def split_cube(parent_cube):
         (parent_min_corner[0], parent_min_corner[1] + half_size[1], parent_min_corner[2] + half_size[2]),
         (parent_min_corner[0] + half_size[0], parent_min_corner[1] + half_size[1], parent_min_corner[2] + half_size[2]),
     ]
+    # Create and return child cubes
     child_cubes = []
-
     for position in child_positions:
         child_cube = OctreeNode(position, half_size)
         child_cubes.append(child_cube)
@@ -62,28 +66,38 @@ def is_point_inside_node(point, node):
     return min_x <= x <= max_x and min_y <= y <= max_y and min_z <= z <= max_z
 
 def calculate_distance(point1, point2):
+    # Calculate the differences in x, y, and z coordinates
     delta_x = point1[0] - point2[0]
     delta_y = point1[1] - point2[1]
     delta_z = point1[2] - point2[2]
+
+    # Calculate the square of the Euclidean distance
     squared_distance = delta_x**2 + delta_y**2 + delta_z**2
+
+    # Take the square root to get the actual distance
     distance = math.sqrt(squared_distance)
-    
+
     return distance
 
 def filter_points_for_node(node, points):
     half_edge_length = min(node.size) / 2
     result = []
+
+    # Create a set to store the indices of points to exclude
     exclude_indices = set()
 
+    # Exclude points that are farther from the node's center than half_edge_length
     for i, point in enumerate(points):
         if calculate_distance(point, node.center) > half_edge_length:
             exclude_indices.add(i)
 
+    # Exclude points that are closer to any child node's center than half_edge_length
     for child_node in node.children:
         for i, point in enumerate(points):
             if calculate_distance(point, child_node.center) > (half_edge_length / 2) and is_point_inside_node(point, child_node):
                 exclude_indices.add(i)
 
+    # Append points to result list that are not in the exclude_indices set
     result.extend(point for i, point in enumerate(points) if i not in exclude_indices)
 
     return result
